b788b145dc7c153f345e3f897eeff1332c613676
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2024-04-11 21:47:48 +0800

    feature: add esp api

---
+++ app/api/esp/get-data/route.ts
@@ -0,0 +1,23 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { compare, hash } from "bcrypt";
+import { signinSchema } from "@/lib/validation";
+import { encrypt } from "@/lib/auth";
+import { cookies } from "next/headers";
+
+export async function GET(req: Request) {
+  try {
+    const esp = await db.espware.findMany()
+
+    return NextResponse.json(
+      { Esp_Data: esp, message: "Extraction Successfull" },
+      { status: 201 }
+    );
+  } catch(error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    )
+  }
+    
+}
