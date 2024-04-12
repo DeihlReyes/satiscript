eca18bf95f0abbf131eccd2d4155b6f7504daddb
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2024-04-12 22:03:15 +0800

    bug: fix bug again

--- app/api/esp/get-data/route.ts
+++ app/api/esp/get-data/route.ts
@@ -7,7 +7,7 @@ import { cookies } from "next/headers";
 
 export async function GET(req: Request) {
   try {
-    const esp = await db.espware.findMany({
+    const esp = await db.espware.findUnique({
       where: { id: "thisisid" },
     })
 
