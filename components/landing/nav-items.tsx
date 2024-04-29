1fa38756dd56ca1265e9304482175b8da4f1bb64
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2024-04-29 22:30:56 +0800

    stlye: add logo on navbar

--- components/landing/nav-items.tsx
+++ components/landing/nav-items.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { headerLinks } from '@/lib/constant';
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '@/public/assets/logo.png'
 
 const NavItems = () => {
     return (
@@ -11,9 +13,12 @@ const NavItems = () => {
                 <>
                     <li
                         key={link.route}
-                        className={`flex-center p-medium-16 whitespace-nowrap transition-all ease-in-out duration-300 border-b-2 border-transparent hover:border-foreground ${index === 2 ? 'md:mx-[5%] border-none' : ''}`}
+                        className={`flex flex-col justify-center items-center whitespace-nowrap transition-all ease-in-out duration-300 border-b-2 border-transparent hover:border-foreground ${index === 2 ? 'md:mx-[5%] border-none' : ''}`}
                     >
-                        <Link className={`${index === 2 ? 'font-extrabold text-2xl' : 'font-semibold'}`} href={link.route}>{link.label}</Link>
+                        <Link className={`flex flex-col justify-center items-center ${index === 2 ? 'font-extrabold text-2xl' : 'font-semibold'}`} href={link.route}>
+                            <Image className={`pb-2 ${index === 2 ? 'block' : 'hidden'}`} src={logo} alt={link.label} width={100} height={100} />
+                            {link.label}
+                        </Link>
                     </li>
                     {(index === 0 || index === 3) && <li className="flex-center p-medium-16">|</li>}
                 </>
